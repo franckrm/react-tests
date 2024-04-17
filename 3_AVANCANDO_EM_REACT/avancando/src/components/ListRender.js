7a260329254651e010c832dde52611d5944e312c
Ribeirovisk <franckrm2001@gmail.com>
2024-04-17 19:40:26 -0300

    learn key property

--- 3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.js
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.js
@@ -4,12 +4,22 @@ const ListRender = () => {
     
     const [list] = useState(["Matheus", "Pedro", "Josias"]);
 
+    const [users] = useState([
+        {id: 1, name: "Matheus", age: 31},
+        {id: 41848, name: "João", age: 28},
+        {id: 1, name: "Pedro", age: 44},
+    ]);
 
     return (
         <div>
             <ul>
-                {list.map((item)=>(
-                    <li>{item}</li>
+                {list.map((item, i)=>(
+                    <li key={i}>{item}</li>
+                ))}
+            </ul>
+            <ul>
+                {users.map((user)=>(
+                    <li key={user.id}>{user.name} - {user.age}</li>
                 ))}
             </ul>
         </div>
